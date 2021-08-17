0efd67c7b071c8a34adf4f9cfa98d6d74f1e5591
jiiaa <jukkaa@live.com>
2021-08-17 16:20:36 +0300

    [WIP] Names of the are posted to a Slack channel.

--- nameday_bot.js
+++ nameday_bot.js
@@ -1,4 +1,4 @@
-const fs = require('fs');
+const request = require('request');
 
 const jsonData = require('./namedays.json');
 
@@ -9,6 +9,24 @@ const month = ('0' + (date_ob.getMonth() + 1)).slice(-2);
 
 const today = month + date;
 const names = jsonData.nameday_list.fi[today].join(', ');
+
+const options = {
+  url: 'https://hooks.slack.com/services/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',
+  json: true,
+  body: {
+    username: 'Nodejs bot',
+    title: 'A title',
+    text: names
+  }
+};
+
+request.post(options, (err, res, body) => {
+  if (err) {
+    console.error('Failed:', err);
+  }
+  console.log('Code: %s\nMessage: %s', res.statusCode, res.statusMessage);
+  console.log('Body:', body);
+});
 
 console.log(names);
 
