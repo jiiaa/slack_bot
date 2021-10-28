c4dbf32584ac6d7245adebf72c654270ab3676e4
Jukka Alkunen <jukka.alkunen@terveystalo.com>
2021-10-28 17:22:54 +0300

    Finnish Swedish names added. Nameday API integrated.

--- daily_improved.js
+++ daily_improved.js
@@ -1,42 +1,50 @@
-require('dotenv').config();
-const axios = require('axios');
-const { WebClient, LogLevel } = require('@slack/web-api');
-const jsonData = require('./namedays.json');
+require("dotenv").config();
+const axios = require("axios");
+const { WebClient, LogLevel } = require("@slack/web-api");
+const jsonData = require("./namedays.json");
 
 // Define the basic settings
-const username = 'Päivän parhaat';
-let emoji = ':nerd_face';
-const channelId = 'C021WNBS5S4'; // #random
-// const channelId = 'C02BQRTKEJ1'; // #slackbot-test
+const username = "Päivän parhaat";
+let emoji = ":nerd_face";
+// const channelId = 'C021WNBS5S4'; // #random
+const channelId = "C02BQRTKEJ1"; // #slackbot-test
 // const channelId = 'C02BXP7S64U'; // #slackbot-dev
 
 // For translating the weekday to Finnish
 const weekdays = {
-  1: 'Maanantai',
-  2: 'Tiistai',
-  3: 'Keskiviikko',
-  4: 'Torstai',
-  5: 'Perjantai',
-  6: 'Lauantai',
-  0: 'Sunnuntai'
+  1: "Maanantai",
+  2: "Tiistai",
+  3: "Keskiviikko",
+  4: "Torstai",
+  5: "Perjantai",
+  6: "Lauantai",
+  0: "Sunnuntai",
 };
 
-// Something fun to post
-const specialDates = {
-  '0910': 'Tänään on Jukan viimeinen päivä redlanderina.',
-  '0913': 'Tänään on Jukan ensimmäinen työpäivä Terveystalolla.',
-  '0914': 'Olli on paras! Tsemii keskusteluun!',
-  '0915': 'Tänään Ilkka tulessa ja liekeissä Solitan golf-kisassa. Tsemii! Voitto kotiin!'
+const getNamesOfDate = async (thisDate) => {
+  const url = `https://api-the-great.herokuapp.com/api/v1/nameday?date=${thisDate}&fi=1&se=1`;
+  const config = {
+    headers: {
+      "x-api-key": process.env.API_KEY,
+    },
+  };
+
+  try {
+    const res = await axios.get(url, config);
+    return res.data;
+  } catch (err) {
+    console.error("error:", err);
+  }
 };
 
 const getDailyChuck = async () => {
   try {
-    const res = await axios.get('https://api.chucknorris.io/jokes/random');
+    const res = await axios.get("https://api.chucknorris.io/jokes/random");
     const chuck = {
-      'mrkdwn_in': ['text'],
-      'color': '#3e3e3e',
-      'title': 'Chuck Norris :muscle:',
-      'text': res.data.value
+      mrkdwn_in: ["text"],
+      color: "#3e3e3e",
+      title: "'Daily Chuck Norris :muscle:",
+      text: res.data.value,
     };
     return chuck;
   } catch (err) {
@@ -45,22 +53,39 @@ const getDailyChuck = async () => {
   }
 };
 
+const getDailyTrump = async () => {
+  try {
+    const res = await axios.get(
+      "https://api.whatdoestrumpthink.com/api/v1/quotes/random"
+    );
+    const trump = {
+      mrkdwn_in: ["text"],
+      color: "#3e3e3e",
+      title: "Daily Donalt Trump :sheep",
+      text: res.data.message,
+    };
+    return trump;
+  } catch (err) {
+    console.error(err);
+    return {};
+  }
+};
+
 const sendMessage = async (text, attachments) => {
   const client = new WebClient(process.env.SLACK_BOT_TOKEN, {
-    logLevel: LogLevel.DEBUG
+    logLevel: LogLevel.DEBUG,
   });
 
   try {
-    const result = await client.chat.postMessage({
+    await client.chat.postMessage({
       channel: channelId,
       username,
       icon_emoji: emoji,
       text,
-      attachments
+      attachments,
     });
-    console.log('result:', result);
   } catch (error) {
-    console.error('Error:', error);
+    console.error("Error:", error);
   }
 };
 
@@ -80,51 +105,59 @@ const dailySlackBot = async () => {
 
   // Count the serial number of today and number of days in this year
   const serialOfDay = Math.floor((date_obj - startOfYear) / 86400000);
-  const daysInYear = Math.floor(((endOfYear - startOfYear) / 86400000) + 1);
+  const daysInYear = Math.floor((endOfYear - startOfYear) / 86400000 + 1);
 
   // Build the text string
   const text = `Tänään on ${weekday} ${date}.${month}.${year}. Päivä ${serialOfDay}/${daysInYear}.`;
 
   // Change the emoji if it is Friday
-  if (date_obj.getDay() === 5)
-    emoji = ':pizza';
+  if (date_obj.getDay() === 5) emoji = ":pizza";
 
   // Need the day and the month in 2-digit value
-  const date2digit = ('0' + date_obj.getDate()).slice(-2);
-  const month2digit = ('0' + (date_obj.getMonth() + 1)).slice(-2);
+  const date2digit = ("0" + date_obj.getDate()).slice(-2);
+  const month2digit = ("0" + (date_obj.getMonth() + 1)).slice(-2);
 
-  // Build the name day string
+  // Get the names of the date from the nameday API
   const today = month2digit + date2digit;
-  const names = jsonData.nameday_list.fi[today].join(', ');
+  const allNames = await getNamesOfDate(today);
 
   // Build the name day text as an array
   let attachments = [];
-  if (names) {
+  if (allNames.fi && allNames.se) {
     attachments = [
       {
-        'mrkdwn_in': ['text'],
-        'color': '#36a64f',
-        'title': 'Nimipäivät tänään',
-        'text': '`Suomalainen kalenteri:` ' + names,
-      }
+        mrkdwn_in: ["text"],
+        color: "#36a64f",
+        title: "Nimipäivät tänään :flag-fi",
+        text: "`Suomenkielinen kalenteri:` " + allNames.fi,
+      },
+      {
+        mrkdwn_in: ["text"],
+        color: "#36a64f",
+        title: "Nimipäivät tänään :flag-se",
+        text: "`Ruotsinkielinen kalenteri:` " + allNames.se,
+      },
     ];
   }
 
   // Check if it is a special day
-  const specialNote = specialDates[today];
-  let specialAttachment = {};
-  if (specialNote) {
-    specialAttachment = {
-      'mrkdwn_in': ['text'],
-      'color': '#0021FF',
-      'title': 'Hox!',
-      'text': specialNote,
-    };
-  }
-  attachments.push(specialAttachment);
+  // const specialNote = specialDates[today];
+  // let specialAttachment = {};
+  // if (specialNote) {
+  //   specialAttachment = {
+  //     mrkdwn_in: ["text"],
+  //     color: "#0021FF",
+  //     title: "Hox!",
+  //     text: specialNote,
+  //   };
+  // }
+  // attachments.push(specialAttachment);
 
   const chuckNorris = await getDailyChuck();
   attachments.push(chuckNorris);
+
+  const donaldTrump = await getDailyTrump();
+  attachments.push(donaldTrump);
 
   sendMessage(text, attachments);
 };
